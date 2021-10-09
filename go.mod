026f484b6665c89392a5d6c66da3b58bb797fb3c
Elysian-Chan <>
2021-10-09 14:20:04 -0700

    Initial.

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/badpetbot/gomodel
+
+go 1.14
+
+require (
+	github.com/badpetbot/gocommon v0.0.0-20211009204726-9acb062fc7f9
+	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
+)
+
+replace github.com/globalsign/mgo => github.com/Nifty255/mgo v0.0.0-20200423052436-ae3b558ebcf4
---
+++ server_member.go
@@ -0,0 +1,108 @@
+package gomodel
+
+import (
+
+  // Import builtin packages.
+  "time"
+
+  // Import 3rd party packages.
+  "github.com/globalsign/mgo"
+  "github.com/globalsign/mgo/bson"
+
+  // Import internal packages.
+  "github.com/badpetbot/gocommon/net"
+  "github.com/badpetbot/gocommon/validation"
+)
+
+// ServerMemberClientName is the name of the MgoDriver to use for ServerMember.
+const ServerMemberClientName = "main"
+
+// ServerMemberDBName is the name of the database to use for ServerMember.
+const ServerMemberDBName = "badpetbot"
+
+// ServerMemberColName is the name of the collection to use for ServerMember.
+const ServerMemberColName = "ServerMembers"
+
+// ServerMemberCol gets a collection reference for ServerMember.
+func ServerMemberCol() *mgo.Collection {
+  return net.MgoCol(ServerMemberClientName, ServerMemberDBName, ServerMemberColName)
+}
+
+// INDICES:
+// { _id: 1 }
+// { discord_id: 1 }
+
+// ServerMember is a single service ServerMember.
+type ServerMember struct {
+  // ID left as the standard BSON ID. DiscordID is the Discord "guild member" ID, which is a "snowflake" string.
+  ID        bson.ObjectId             `bson:"_id"                   json:"_id"                    validate:"required"`
+  DiscordID string                    `bson:"discord_id"            json:"discord_id"             validate:"required"`
+  CreatedAt time.Time                 `bson:"created_at"            json:"created_at"             validate:"required"`
+  UpdatedAt time.Time                 `bson:"updated_at"            json:"updated_at"             validate:"required"`
+
+  // Ownership relationships
+  OwnerDiscordID      string          `bson:"owner_discord_id"      json:"owner_discord_id"       validate:"-"`
+  SecOwnerDiscordIDs  []string        `bson:"sec_owner_discord_ids" json:"sec_owner_discord_ids"  validate:"-"`
+
+  // Embeddables
+
+  Owner               *ServerMember   `bson:"owner,omitalways"      json:"owner"                  validate:"-"`
+  SecOwners           []ServerMember  `bson:"sec_owners,omitalways" json:"sec_owners"             validate:"-"`
+}
+
+// Create persists the document in the database. It can optionally run validations if present and
+// prevent model persistence if they do not pass.
+func (this *ServerMember) Create() error {
+
+  // Ensure ID, timestamps, and tokens.
+  this.ID = bson.NewObjectId()
+  now := time.Now()
+  this.CreatedAt = now
+  this.UpdatedAt = now
+
+  // Ensure defaults.
+
+  // Run validations and return if they fail.
+  if err := this.Validate(); err != nil {
+    return err
+  }
+
+  // Persist the ServerMember.
+  return net.MgoCol(ServerMemberClientName, ServerMemberDBName, ServerMemberColName).Insert(this)
+}
+
+// Update updates the document in the database. Important note, this function does NOT prepend
+// the provided updates with "$set" or any other operator.
+func (this *ServerMember) Update(updates bson.M) error {
+
+  // Update updated-at timestamp.
+  this.UpdatedAt = time.Now()
+  _, setting := updates["$set"]
+  if !setting {
+    updates["$set"] = bson.M{}
+  }
+  updates["$set"].(bson.M)["updated_at"] = this.UpdatedAt
+
+  if err := this.Validate(); err != nil {
+    return err
+  }
+
+  // Persist the updates.
+  return net.MgoCol(ServerMemberClientName, ServerMemberDBName, ServerMemberColName).UpdateId(this.ID, updates)
+}
+
+// Delete permanently removes the document from the database.
+func (this *ServerMember) Delete() error {
+
+  // Delete the Link.
+  return net.MgoCol(ServerMemberClientName, ServerMemberDBName, ServerMemberColName).RemoveId(this.ID)
+}
+
+// Validate runs validations against the model's fields.
+func (this *ServerMember) Validate() error {
+
+  // Implement validation rules here.
+  return validation.NewValidator().Struct(this)
+}
+
+// Misc functions.
